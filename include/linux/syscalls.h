5ed46af4f591fbe7ba0fafbf20121f1548ceb45f
cxia <xia38832@yahoo.com>
2012-09-26 12:24:16 -0700

    second commit

--- include/linux/syscalls.h
+++ include/linux/syscalls.h
@@ -860,4 +860,7 @@ asmlinkage long sys_process_vm_writev(pid_t pid,
 
 asmlinkage long sys_kcmp(pid_t pid1, pid_t pid2, int type,
 			 unsigned long idx1, unsigned long idx2);
+
+asmlinkage int sys_hello_world(void);
+
 #endif
